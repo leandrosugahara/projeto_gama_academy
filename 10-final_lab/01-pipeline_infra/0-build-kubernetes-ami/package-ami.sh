9808db26c08706d27958d5cafb2f60a37c353a8f
leandsu <leandro.sugahara@gmail.com>
2021-11-24 13:54:22 +0000

    final lab

--- 10-final_lab/01-pipeline_infra/0-build-kubernetes-ami/package-ami.sh
+++ 10-final_lab/01-pipeline_infra/0-build-kubernetes-ami/package-ami.sh
@@ -2,8 +2,12 @@
 
 VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
 
+echo "Versao: " $VERSAO
+
 cd 10-final_lab/01-pipeline_infra/0-build-kubernetes-ami/0-terraform
 RESOURCE_ID=$(terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
+
+echo "Resource ID: " $RESOURCE_ID
 
 cd ../2-terraform-ami
 terraform init
