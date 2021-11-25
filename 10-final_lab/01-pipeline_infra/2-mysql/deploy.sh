f58847b4c7730c2283ac1f57cafb3b8c120473d4
leandsu <leandro.sugahara@gmail.com>
2021-11-25 09:00:26 +0000

    final lab

--- 10-final_lab/01-pipeline_infra/2-mysql/deploy.sh
+++ 10-final_lab/01-pipeline_infra/2-mysql/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#cd 10-final_lab/01-pipeline_infra/2-mysql/0-terraform
-cd 0-terraform
+cd 10-final_lab/01-pipeline_infra/2-mysql/0-terraform
+# cd 0-terraform
 terraform init
 terraform apply -auto-approve
 
