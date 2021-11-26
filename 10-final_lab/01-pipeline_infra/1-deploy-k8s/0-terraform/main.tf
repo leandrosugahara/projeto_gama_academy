3be63103766d7e4809195ebbf6db5983994a162d
leandsu <leandro.sugahara@gmail.com>
2021-11-26 17:04:59 +0000

    final lab

--- 10-final_lab/01-pipeline_infra/1-deploy-k8s/0-terraform/main.tf
+++ 10-final_lab/01-pipeline_infra/1-deploy-k8s/0-terraform/main.tf
@@ -188,24 +188,23 @@ resource "aws_security_group" "acessos_g4_haproxy" {
     #   self             = false
     #   to_port          = 0
     # },
+    # {
+    #   cidr_blocks      = []
+    #   description      = ""
+    #   from_port        = 0
+    #   ipv6_cidr_blocks = []
+    #   prefix_list_ids  = []
+    #   protocol         = "tcp"
+    #   security_groups  = []
+    #   self             = true
+    #   to_port          = 65535
+    # },
     {
-      cidr_blocks      = []
-      description      = ""
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "Libera todos acessos"
       from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = true
-      to_port          = 65535
-    },
-    {
-      cidr_blocks      = []
-      description      = ""
-      from_port        = 0
-      protocol         = "all"
-      security_groups  = []
       to_port          = 0
+      protocol         = "all"
     },
   ]
 
